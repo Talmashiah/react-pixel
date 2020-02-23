f2937e0353b74f00d40ad89369c3ee86351f685c
talmashiah <talmashiah@gmail.com>
2020-02-23 02:08:41 +0200

    Updates

---
+++ precache-manifest.b25daeafac82a021d7976e7e1025bd41.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8521547585016bc635b7edcca9ecd38",
+    "url": "/react-pixel/index.html"
+  },
+  {
+    "revision": "f17a0bb1d32afdfdfc2f",
+    "url": "/react-pixel/static/css/main.c5b60f3b.chunk.css"
+  },
+  {
+    "revision": "554731f53984fa8a691d",
+    "url": "/react-pixel/static/js/2.939966ae.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-pixel/static/js/2.939966ae.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f17a0bb1d32afdfdfc2f",
+    "url": "/react-pixel/static/js/main.ed9e656a.chunk.js"
+  },
+  {
+    "revision": "885b4ecc335cc1131d1e",
+    "url": "/react-pixel/static/js/runtime-main.c82c7be1.js"
+  },
+  {
+    "revision": "fddc5fa6efb91e49befb6ad849da9b0f",
+    "url": "/react-pixel/static/media/Livvic-Bold.fddc5fa6.ttf"
+  },
+  {
+    "revision": "9939ebf90a63343d97a1f13099d07068",
+    "url": "/react-pixel/static/media/Livvic-MediumItalic.9939ebf9.ttf"
+  },
+  {
+    "revision": "fe84dbc6ce317e6216d959cf7baed69c",
+    "url": "/react-pixel/static/media/Livvic-SemiBold.fe84dbc6.ttf"
+  },
+  {
+    "revision": "e45a3d0d8de698bfbea8ddc00225fd8e",
+    "url": "/react-pixel/static/media/about-circle-left.e45a3d0d.svg"
+  },
+  {
+    "revision": "2fd804c4ec4e139d392e43c599bd277b",
+    "url": "/react-pixel/static/media/about-circle-right.2fd804c4.svg"
+  },
+  {
+    "revision": "d1d950e09ee94a2bbdd4627e65143cbc",
+    "url": "/react-pixel/static/media/avatar-aiysha.d1d950e0.jpg"
+  },
+  {
+    "revision": "60ff8909e4d598e25c144f0b47d988e3",
+    "url": "/react-pixel/static/media/avatar-arthur.60ff8909.jpg"
+  },
+  {
+    "revision": "f16826cacf2b1a460d2eab969d343d0a",
+    "url": "/react-pixel/static/media/avatar-kady.f16826ca.jpg"
+  },
+  {
+    "revision": "1f80fc3fb7d03dd4d7a60927c76a30f2",
+    "url": "/react-pixel/static/media/circle.1f80fc3f.svg"
+  },
+  {
+    "revision": "1ece178695707c353a82b30a19b11d2e",
+    "url": "/react-pixel/static/media/icon-chart.1ece1786.svg"
+  },
+  {
+    "revision": "6cb0f5760e3e8a0f4567755fb5250b32",
+    "url": "/react-pixel/static/media/icon-cog.6cb0f576.svg"
+  },
+  {
+    "revision": "7febcff9f143b83553d544fc04fc5b09",
+    "url": "/react-pixel/static/media/icon-facebook.7febcff9.svg"
+  },
+  {
+    "revision": "05759150ed61077a65185636252594d7",
+    "url": "/react-pixel/static/media/icon-person.05759150.svg"
+  },
+  {
+    "revision": "c557c3d4e80b5eda1ae120c8c2257e26",
+    "url": "/react-pixel/static/media/icon-pinterest.c557c3d4.svg"
+  },
+  {
+    "revision": "cb4fea0b4771a5dfc70f787d5e01c91e",
+    "url": "/react-pixel/static/media/icon-twitter.cb4fea0b.svg"
+  },
+  {
+    "revision": "fe4f785a6475f2ea47465eb9a6fcda38",
+    "url": "/react-pixel/static/media/left-circle.fe4f785a.svg"
+  },
+  {
+    "revision": "00dc85d0f8c874f627cedb798221c9c5",
+    "url": "/react-pixel/static/media/nav-circle.00dc85d0.svg"
+  },
+  {
+    "revision": "458c9fcdfef1f53beea9819bd3a2b4e8",
+    "url": "/react-pixel/static/media/right-circle.458c9fcd.svg"
+  },
+  {
+    "revision": "8fced9299fa222aa1131f36e3d689453",
+    "url": "/react-pixel/static/media/skills-circle.8fced929.svg"
+  }
+]);
